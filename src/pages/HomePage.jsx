b1bcbb7ba591a8767c0cd43e2e8935f8f7561a98
1kxk <caiow.wk@gmail.com>
2020-08-10 05:16:12 -0300

    fix homepage wallpaper

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 
 const HomePage = () => {
   return (
-    <div
-      style={{
-        width: '100%',
-        backgroundImage:
-          'url("https://images6.alphacoders.com/909/thumb-1920-909641.png")',
-        height: '100vh',
-        backgroundSize: 'cover',
-      }}
-    ></div>
+    <div>
+      <img
+        style={{
+          width: '100vw',
+          height: '100vh',
+          backgroundSize: 'cover',
+        }}
+        className="background-image"
+        src="https://images6.alphacoders.com/909/thumb-1920-909641.png"
+        alt="rick and morty"
+      />
+    </div>
   );
 };
 
