852da978ec798ab5a05cdfcd366a46e3d5ad583f
1kxk <caiow.wk@gmail.com>
2020-08-10 05:18:07 -0300

    Updates

---
+++ precache-manifest.16f572d0c3305c8cfd5dfed6743d921e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f00d7fe365a660e2e370834ea5a405f",
+    "url": "/Rick-And-Morty/index.html"
+  },
+  {
+    "revision": "710a83662653be0e56b0",
+    "url": "/Rick-And-Morty/static/css/main.27673896.chunk.css"
+  },
+  {
+    "revision": "ca0af0dd8ce51543f600",
+    "url": "/Rick-And-Morty/static/js/2.88008445.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Rick-And-Morty/static/js/2.88008445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "710a83662653be0e56b0",
+    "url": "/Rick-And-Morty/static/js/main.0417554d.chunk.js"
+  },
+  {
+    "revision": "9869b056805a93a86c8e",
+    "url": "/Rick-And-Morty/static/js/runtime-main.9f2c9de1.js"
+  }
+]);
