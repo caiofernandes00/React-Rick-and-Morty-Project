bfe8899af20179b9a97fada8106ab04d6d793254
1kxk <caiow.wk@gmail.com>
2020-08-10 11:22:28 -0300

    Updates

---
+++ precache-manifest.9895fd4026205713190be28102ecfbf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b865eeb9b891a936a1180b30487d923b",
+    "url": "/Rick-and-Morty/index.html"
+  },
+  {
+    "revision": "710a83662653be0e56b0",
+    "url": "/Rick-and-Morty/static/css/main.27673896.chunk.css"
+  },
+  {
+    "revision": "ca0af0dd8ce51543f600",
+    "url": "/Rick-and-Morty/static/js/2.88008445.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Rick-and-Morty/static/js/2.88008445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "710a83662653be0e56b0",
+    "url": "/Rick-and-Morty/static/js/main.0417554d.chunk.js"
+  },
+  {
+    "revision": "e55599ef5ca3878013e0",
+    "url": "/Rick-and-Morty/static/js/runtime-main.7fd533d3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Rick-And-Morty/precache-manifest.1dd20f651eb5019bee1cab115cd5c318.js"
+  "/Rick-and-Morty/precache-manifest.9895fd4026205713190be28102ecfbf3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Rick-And-Morty/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Rick-and-Morty/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
